4984630b254332bd095be14a968342dc84985612
joao victor <jvictor@a5solutions.com>
2026-03-01 18:46:17 -0300

    feat: get a single pet use case

---
+++ src/application/use-cases/pet/get-pet.spec.ts
@@ -0,0 +1,51 @@
+import { ResourceNotFoundError } from "@/application/error/resource-not-found-error";
+import { Pet } from "@/domain/pet/entities/pet";
+import { PetDependency } from "@/domain/pet/value-object/pet-dependence";
+import { PetEnergy } from "@/domain/pet/value-object/pet-energy";
+import { PetSize } from "@/domain/pet/value-object/pet-size";
+import { InMemoryPetRepository } from "@/infra/repositories/in-memory/pet-in-memory-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { GetPetUseCase } from "./get-pet";
+
+describe("get pet use case", () => {
+    let sut: GetPetUseCase
+    let petRepository: InMemoryPetRepository
+    beforeEach(() => {
+        petRepository = new InMemoryPetRepository()
+        sut = new GetPetUseCase({
+            repositories: {
+                pets: petRepository
+            }
+        })
+    })
+
+    it("should  find a pet", async () => {
+        const petId = "pet-id"
+        const pet = Pet.create({
+            city: "selectedCity",
+            state: "selectedState",
+            dependence: "high" as PetDependency,
+            energy: "high" as PetEnergy,
+            size: "medium" as PetSize,
+            id: petId,
+            name: "pet-name",
+            orgId: "org-id",
+        });
+
+        await petRepository.create(pet)
+
+        const response = await sut.execute({
+            id: "pet-id"
+        })
+
+        expect(response.pet.id).toBe(petId)
+    })
+
+    it("should trow a ResourceNotFoundError if pet din't exist", async () => {
+        const petId = "pet-id"
+
+
+        expect(sut.execute({ id: petId })).rejects.toBeInstanceOf(ResourceNotFoundError)
+
+    })
+})
---
+++ src/application/use-cases/pet/get-pet.ts
@@ -0,0 +1,34 @@
+import { GetPetCommand, GetPetCommandResponse } from "@/application/dto/pet/get-pet-command";
+import { ResourceNotFoundError } from "@/application/error/resource-not-found-error";
+import { PetResponseMapper } from "@/application/mappers/pet-response-mapper";
+import { PetRepository } from "@/domain/pet/repositories/pet-repository";
+
+interface Repositories {
+    pets: PetRepository;
+}
+
+interface GetPetUseCaseDeps {
+    repositories: Repositories;
+}
+
+
+export class GetPetUseCase {
+    private pets: PetRepository;
+    constructor(private readonly deps: GetPetUseCaseDeps) {
+        this.pets = deps.repositories.pets;
+    }
+
+    async execute(props: GetPetCommand): Promise<GetPetCommandResponse> {
+        const pet = await this.pets.findById(props.id);
+        if (pet === null) {
+            throw new ResourceNotFoundError("pet");
+        }
+
+        const petMapper = PetResponseMapper.toDTO(pet)
+
+        return {
+            pet: petMapper
+        }
+
+    }
+}
